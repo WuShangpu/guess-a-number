e1eb0efe75c3830def6ee9a8a75ad6ecc3401689
wushangpu <wshangpu@gmail.com>
2018-03-30 21:13:57 +0800

    add files

--- functions.py
+++ functions.py
@@ -1,17 +1,33 @@
 # -*- coding:utf-8 -*-
-#猜数字大小的函数
+from setting import Setting
+import sys
 
+f_settings = Setting()
 
-def guess():
+#判断是否继续游戏
+def go_on_game():
+	if f_settings.switch == False:
+		go = input('Do you want play agian? YES or NO? ')
+		if go == 'YES' or 'yes':
+			guess(f_settings.num)
+		elif go == 'NO' or 'no':
+			sys.exit()
+		
 	
-	while switch:
+		
+#猜数字大小的函数
+def guess(num):
+	while f_settings.switch:
 		ans = int(input('Please input a number:'))
 		if ans > num:
 			print('Too big!')
-			switch = True
+			f_settings.switch = True
 		elif ans < num:
 			print('Too small!')
-			switch = True
+			f_settings.switch = True
 		else:
 			print('Bingo!')
-			switch = False
+			f_settings.switch = False
+			
+
+	
--- guess-a-number.py
+++ guess-a-number.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 #猜数字游戏升级版
-import random
-import time
+
 import pygame
 import functions as f
 from setting import Setting
@@ -10,10 +9,11 @@ from setting import Setting
 def run_game():
 	gn_settings = Setting()
 	print('Let\'s begin the game!')
-	time.sleep(1)
-	while begin:
-		f.guess()
-		print('Do you want to play agian?')
+	#开始游戏主循环
+	while gn_settings.begin:
+		f.guess(gn_settings.num)
+		f.go_on_game()
+		
 		
 
 run_game()
